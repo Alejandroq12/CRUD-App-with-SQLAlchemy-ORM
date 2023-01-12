8e5ecdef0e0803a0ba97b9680c7b689489aed0e4
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-01-12 00:31:47 -0600

    Making the connnection to the database with SQLAlchemy

--- flask-hello-app.py
+++ flask-hello-app.py
@@ -1,7 +1,10 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:4991@localhost:5432/postgres'
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
