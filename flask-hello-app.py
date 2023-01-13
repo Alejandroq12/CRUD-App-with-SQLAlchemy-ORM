df3aa658d556443466cbad8f9ed00efaee258062
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-01-12 21:02:57 -0600

    feat
    Adding a connection to a database

--- flask-hello-app.py
+++ flask-hello-app.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:4991@localhost:5432/postgres'
 db = SQLAlchemy(app)
 
+class Person(db.Model):
+    __tablename__ = 'persons'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+
 
 @app.route('/')
 def index():
