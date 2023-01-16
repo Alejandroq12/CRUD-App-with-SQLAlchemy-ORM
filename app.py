7fc9eb172ec6815961feb6eabeba8bb15f767545
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-01-15 20:27:35 -0600

    Refactor: Adding SQLALCHEMY_TRACK_MODIFICATIONS
    SQLALCHEMY_TRACK_MODIFICATIONS is a configuration option for the Flask-SQLAlchemy extension for the Flask web Framework. It is used to indicate whether or not SQLAlchemy should track modifications to objects and emit signals.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:4991@localhost:5432/postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
