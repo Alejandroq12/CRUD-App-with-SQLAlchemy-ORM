640474aeb7f131271386acfdf1563c0b0dee595a
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-01-23 17:17:20 -0600

    Refactor: bilding a complete functional web app
    This a better code in which I changed database, I wrote better code, I used separation of concerns, I used Post and Get methods, I used Migrations and I used SQLAlchemy to modify my database

--- app.py
+++ app.py
@@ -1,28 +1,36 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
-import sqlalchemy as sa
-from flask_migrate import Migrate
 import sys
-from flask import abort
-from flask import jsonify
+from flask_migrate import Migrate
 
 
 app = Flask(__name__, static_folder='static')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:4991@localhost:5432/todoapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:4991@localhost:5432/crudapp5'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-
 class Todo(db.Model):
   __tablename__ = 'todos'
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String(), nullable=False)
-  completed = db.Column(db.Boolean, nullable=False, default=False)
+  completed = db.Column(db.Boolean, nullable=False)
+  list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
+
 
   def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
+
+# with app.app_context():
+#   db.create_all()
+
+class TodoList(db.Model):
+  __tablename__ = 'todolists'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(), nullable=False)
+  todos = db.relationship('Todo', backref='List', lazy=True)
+
 
 
 @app.route('/todos/create', methods=['POST'])
@@ -31,9 +39,11 @@ def create_todo():
   body = {}
   try:
     description = request.get_json()['description']
-    todo = Todo(description=description)
+    todo = Todo(description=description, completed=False)
     db.session.add(todo)
     db.session.commit()
+    body['id'] = todo.id
+    body['completed'] = todo.completed
     body['description'] = todo.description
   except:
     error = True
@@ -47,9 +57,34 @@ def create_todo():
     return jsonify(body)
 
 
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+  try:
+    completed = request.get_json()['completed']
+    print('completed', completed)
+    todo = Todo.query.get(todo_id)
+    todo.completed = completed
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return redirect(url_for('index'))
+
+@app.route('/todos/<todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+  try:
+    Todo.query.filter_by(id=todo_id).delete()
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return jsonify({ 'success': True})
+
 @app.route('/')
 def index():
-  return render_template('index.html', data=Todo.query.all())
+  return render_template('index.html', data=Todo.query.order_by('id').all())
 
 
 if __name__ == '__main__':
--- static/script.js
+++ static/script.js
@@ -1,23 +1,57 @@
-const descInput = document.getElementById('description');
+const checkboxes = document.querySelectorAll('.check-completed');
+for (let i = 0; i < checkboxes.length; i++) {
+  const checkbox = checkboxes[i];
+  checkbox.onchange = function(e) {
+    const newCompleted = e.target.checked;
+    const todoId = e.target.dataset['id'];
+    fetch('/todos/' + todoId + '/set-completed', {
+      method: 'POST',
+      body: JSON.stringify({
+        'completed': newCompleted
+    }),
+    headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(function() {
+      document.getElementById('error').className = 'hidden';
+    })
+    .catch(function() {
+      document.getElementById('error').className = '';
+    })
+  }
+}
+
+const deleteBtns = document.querySelectorAll('.delete-button');
+    for (let i = 0; i < deleteBtns.length; i++) {
+        const btn = deleteBtns[i];
+        btn.onclick = function(e) {
+            const todoId = e.target.dataset['id'];
+            fetch('/todos/' + todoId, {
+                method: 'DELETE'
+            });
+        }
+    }
+
 document.getElementById('form').onsubmit = function(e) {
   e.preventDefault();
-  const desc = descInput.value;
-  descInput.value = '';
   fetch('/todos/create', {
     method: 'POST',
     body: JSON.stringify({
-      'description': desc,
+      'description': document.getElementById('description').value
     }),
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/json'
     }
   })
-  .then(response => response.json())
-  .then(jsonResponse => {
-    console.log('response', jsonResponse);
-    li = document.createElement('li');
-    li.innerText = desc;
-    document.getElementById('todos').appendChild(li);
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(jsonResponse) {
+    console.log(jsonResponse);
+    const liItem = document.createElement('LI');
+    liItem.innerHTML = jsonResponse['description'];
+    document.getElementById('todos').appendChild(liItem);
     document.getElementById('error').className = 'hidden';
   })
   .catch(function() {
